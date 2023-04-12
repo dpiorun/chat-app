953a6fd8928318e01dcc4835f0b8c64cc1d4858e
Damian Piorun <damian.piorun@gmail.com>
2023-04-12 19:40:27 +0200

    chore: remove unused information from backendTypes

--- components/Chat/__tests__/Chat.test.tsx
+++ components/Chat/__tests__/Chat.test.tsx
@@ -6,7 +6,6 @@ import Chat from "../Chat";
 const mockUser: User = {
   gender: "string",
   name: {
-    title: "string",
     first: "string",
     last: "string",
   },
@@ -19,39 +18,14 @@ const mockUser: User = {
     state: "string",
     country: "string",
     postcode: 123,
-    coordinates: {
-      latitude: "string",
-      longitude: "string",
-    },
-    timezone: {
-      offset: "string",
-      description: "string",
-    },
   },
-  email: "string",
   login: {
     uuid: crypto.getRandomValues(new Uint32Array(10)).toString(),
-    username: "string",
-    password: "string",
-    salt: "string",
-    md5: "string",
-    sha1: "string",
-    sha256: "string",
   },
   dob: {
     date: "string",
-    age: 123,
-  },
-  registered: {
-    date: "string",
-    age: 123,
   },
   phone: "string",
-  cell: "string",
-  id: {
-    name: "string",
-    value: null,
-  },
   picture: {
     large: "string",
     medium: "string",
